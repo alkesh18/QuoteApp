4a5a29cf1024088daf7a1cebe25aed23f259f105
Alkesh Sandal <alkesh18@hotmail.com>
2020-09-26 23:01:54 -0400

    added pages

---
+++ src/app/tab2/pages/view-quote/view-quote.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-quote',
+  templateUrl: './view-quote.page.html',
+  styleUrls: ['./view-quote.page.scss'],
+})
+export class ViewQuotePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
