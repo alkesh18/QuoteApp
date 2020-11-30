6fa85ce96753bf8ec9f9c022bc5cddd9f5ce5c8b
Ebrahim Zhalehsani <zhalehsa@sheridancollege.ca>
2020-11-30 18:17:06 -0500

    IPUpdated

--- src/app/services/nodejs.service.ts
+++ src/app/services/nodejs.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from   '@angular/core';
+import { HttpClient } from   '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,27 +8,29 @@ export class NodejsService {
 
   constructor(private http: HttpClient) { }
 
+  ip : string = "127.0.0.1:8887";
+
   insert(params) {
-    return this.http.post('http://127.0.0.1:8887/quotes/',  {params} );
+    return this.http.post(`http://${this.ip}/quotes/`,  {params} );
   }
 
   update(params) {
-    return this.http.patch('http://127.0.0.1:8887/quotes/updateQuote/', { params });
+    return this.http.patch(`http://${this.ip}/quotes/updateQuote/`, { params });
   }
 
   retrieve(params) {
-    return this.http.get('http://127.0.0.1:8887/quotes/selectQuote/', { params });
+    return this.http.get(`http://${this.ip}/quotes/selectQuote/`, { params });
   }
 
   retrieveAll(params) {
     
-    return this.http.get('http://127.0.0.1:8887/quotes/', {params});
+    return this.http.get(`http://${this.ip}/quotes/`, {params});
   }
   retrieveAllWithNoID() {
-    return this.http.get('http://127.0.0.1:8887/quotes/getAllNoID/');
+    return this.http.get(`http://${this.ip}/quotes/getAllNoID/`);
   }
 
   deleteQuote(params) {
-    return this.http.delete('http://127.0.0.1:8887/quotes/deleteQuote/', { params });
+    return this.http.delete(`http://${this.ip}/quotes/deleteQuote/`, { params });
   }
 }
